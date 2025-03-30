ddece97fe74cd4768f7c882a2b34e3ddca6080a0
Adly17 <3adly1710@gmail.com>
2025-03-30 23:04:22 +0200

    updated application.properties abd pom.xml of each microservice

--- jobms/jobms/src/main/java/com/embarkx/jobms/job/Implement/JobServiceImplementation.java
+++ jobms/jobms/src/main/java/com/embarkx/jobms/job/Implement/JobServiceImplementation.java
@@ -3,10 +3,15 @@ package com.embarkx.jobms.job.Implement;
 import com.embarkx.jobms.job.Job;
 import com.embarkx.jobms.job.JobRepository;
 import com.embarkx.jobms.job.JobService;
+import com.embarkx.jobms.job.dto.JobWithCompanyDTO;
+import com.embarkx.jobms.job.external.Company;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class JobServiceImplementation implements JobService {
@@ -18,10 +23,29 @@ public class JobServiceImplementation implements JobService {
     }
 
     @Override
-    public List<Job> findAll() {
-        return jobRepository.findAll();
+    public List<JobWithCompanyDTO> findAll() {
+        List<Job> jobs = jobRepository.findAll();
+        List<JobWithCompanyDTO> jobWithCompanyDTOs = new ArrayList<>();
+
+
+
+
+
+
+
+        return jobs.stream().map(this::convertToDTO)
+                .collect(Collectors.toList());
     }
 
+    private JobWithCompanyDTO convertToDTO(Job job){
+            JobWithCompanyDTO jobWithCompanyDTO = new JobWithCompanyDTO();
+            jobWithCompanyDTO.setJob(job);
+
+        RestTemplate restTemplate = new RestTemplate();
+            Company company = restTemplate.getForObject("http://localhost:8081/companies/" + job.getCompanyId(), Company.class);
+            jobWithCompanyDTO.setCompany(company);
+            return jobWithCompanyDTO;
+    }
     @Override
     public void createJob(Job job) {
         jobRepository.save(job);
